1b7ad398343b5d618c278c74434a1215163c43c0
Z3R0O2 <91497379+Z3R0O2@users.noreply.github.com>
2022-03-10 16:54:09 +0700

    request info function

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import requests
 import json
 from pprint import pprint
 
+#reuqest info
+def get_info(call):
+    r = requests.get(call)
+    return r.json()
+
 #Get API_KEY
 API_FILE = open("API.json", "r")
 API_KEY = json.loads(API_FILE.read())["API-KEY"]
 
+UUID = ""
+player_url = "https://api.hypixel.net/skyblock"
