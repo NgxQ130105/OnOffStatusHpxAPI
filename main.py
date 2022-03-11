fc8642f2529ccb04994d2e24d71cecb33bbcfabf
Z3R0O2 <91497379+Z3R0O2@users.noreply.github.com>
2022-03-11 18:40:07 +0700

    YESSS IT WORKSSSSSS
    ahh finally, adding it discord later

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import requests
 from mojang import MojangAPI
+import time
 
-loginCondition = True
-while loginCondition == True:
-    api_key = "00297e7c-8c63-4e7c-8ce3-aa8a419e3fd0"
+api_key = "00297e7c-8c63-4e7c-8ce3-aa8a419e3fd0"
 
-    userinput = "z3r0o2"
-    uuid = MojangAPI.get_uuid(userinput)
-    requestlink = f"https://api.hypixel.net/player?key={api_key}&uuid={uuid}"
+userinput = "z3r0o2"
+uuid = MojangAPI.get_uuid(userinput)
+requestlink = f"https://api.hypixel.net/player?key={api_key}&uuid={uuid}"
 
+hydata = requests.get(requestlink).json()
+
+
+#Main runtime
+while True:
     hydata = requests.get(requestlink).json()
 
     player = hydata["player"]["displayname"]
 
     logoutime = hydata['player']['lastLogout']
     logintime = hydata['player']['lastLogin']
-
-    while loginCondition == True:
-        if logoutime < logintime:
-            loginCondition = True
-        else:
-            loginCondition = False
-            print(f"{player} is no longer online")
+    
+    if logoutime > logintime:
+        print(f"{player} is no longer online")
+        break
+    time.sleep(60)
+    
