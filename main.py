d8e0f102da3fafff297b40ab5da27516ab580f02
Z3R0O2 <91497379+Z3R0O2@users.noreply.github.com>
2022-03-12 12:34:44 +0700

    Creating Virutal Environment + Adding Hikari + Lightbulb
    Still an incomplete test -> Not recommended for download!

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+#Import libs
 import requests
 from mojang import MojangAPI
 import time
+import hikari
+import lightbulb
 
+
+bot = hikari.GatewayBot(token="OTUyMDU4MjM4Nzc2NTEyNjAz.YiwfVA.vCQO21Xb_NNBlhGwY6g3xt8uQCg" , 
+    default_enabled_guild = (952060009863344148)
+)
+
+
+@bot.listen(hikari.StartedEvent)
+async def bot_started(event):
+    print('Bot has started')
+
+@bot.command
+@lightbulb.command('ping', 'Says pong!')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond('Pong')
+bot.run()
 api_key = "00297e7c-8c63-4e7c-8ce3-aa8a419e3fd0"
 
 userinput = "z3r0o2"
