fae17b4ecf82bc95dc56c23f1f8baad5f7cd3e4f
Z3R0O2 <91497379+Z3R0O2@users.noreply.github.com>
2022-03-13 11:09:05 +0700

    Update OnHypixel and OnSkyblock Request lines
    Testing out how Hypixel API provides infos

--- main.py
+++ main.py
@@ -6,41 +6,48 @@ import hikari
 import lightbulb
 
 
-bot = hikari.GatewayBot(token="OTUyMDU4MjM4Nzc2NTEyNjAz.YiwfVA.vCQO21Xb_NNBlhGwY6g3xt8uQCg" , 
-    default_enabled_guild = (952060009863344148)
-)
+# bot = hikari.GatewayBot(token="O#TUyMDU4MjM4Nzc2NTEyNjAz.YiwfVA.0l7tFVRgM-Le0bhK0e-7RxxUuwk" , 
+#     default_enabled_guild = (952060009863344148)
+# )
 
 
-@bot.listen(hikari.StartedEvent)
-async def bot_started(event):
-    print('Bot has started')
+# @bot.listen(hikari.StartedEvent)
+# async def bot_started(event):
+#     print('Bot has started')
 
-@bot.command
-@lightbulb.command('ping', 'Says pong!')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def ping(ctx):
-    await ctx.respond('Pong')
-bot.run()
+# @bot.command
+# @lightbulb.command('ping', 'Says pong!')
+# @lightbulb.implements(lightbulb.SlashCommand)
+# async def ping(ctx):
+#     await ctx.respond('Pong')
+# bot.run()
+
 api_key = "00297e7c-8c63-4e7c-8ce3-aa8a419e3fd0"
 
 userinput = "z3r0o2"
+
 uuid = MojangAPI.get_uuid(userinput)
-requestlink = f"https://api.hypixel.net/player?key={api_key}&uuid={uuid}"
+requestStatus = f"https://api.hypixel.net/status?key={api_key}&uuid={uuid}"
 
-hydata = requests.get(requestlink).json()
+hydata = requests.get(requestStatus).json()
+onHypixel = hydata["session"]["online"]
+onSkyblock = hydata["session"]["gameType"]
+
+print(onSkyblock)
+print(onSkyblock)
 
 
-#Main runtime
-while True:
-    hydata = requests.get(requestlink).json()
+# #Main runtime
+# while True:
+#     hydata = requests.get(requestlink).json()
 
-    player = hydata["player"]["displayname"]
+#     player = hydata["player"]["displayname"]
 
-    logoutime = hydata['player']['lastLogout']
-    logintime = hydata['player']['lastLogin']
+#     logoutime = hydata['player']['lastLogout']
+#     logintime = hydata['player']['lastLogin']
     
-    if logoutime > logintime:
-        print(f"{player} is no longer online")
-        break
-    time.sleep(60)
+#     if logoutime > logintime:
+#         print(f"{player} is no longer online")
+#         break
+#     time.sleep(60)
     
