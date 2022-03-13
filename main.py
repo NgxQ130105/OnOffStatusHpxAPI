c81ecd50eb0ae6261e827050804ab8f55d94405c
Z3R0O2 <91497379+Z3R0O2@users.noreply.github.com>
2022-03-13 11:42:45 +0700

    Remaking the main function and loop!
    I just revamped the main lines! After the test, they turned out the code worked well so I'm on my way to implement them into the Discord bot!

--- main.py
+++ main.py
@@ -23,31 +23,38 @@ import lightbulb
 # bot.run()
 
 api_key = "00297e7c-8c63-4e7c-8ce3-aa8a419e3fd0"
+userinput = "z3r0o2" #or input()
 
-userinput = "z3r0o2"
 
+#Getting the Hpx ID
 uuid = MojangAPI.get_uuid(userinput)
 requestStatus = f"https://api.hypixel.net/status?key={api_key}&uuid={uuid}"
 
-hydata = requests.get(requestStatus).json()
-onHypixel = hydata["session"]["online"]
-onSkyblock = hydata["session"]["gameType"]
-
-print(onSkyblock)
-print(onSkyblock)
 
 
-# #Main runtime
-# while True:
-#     hydata = requests.get(requestlink).json()
+# #FOR TESTING PURPOSE
+# hydata = requests.get(requestStatus).json()
+# onHypixel = hydata["session"]["online"]
+# onSkyblock = hydata["session"]["gameType"]
 
-#     player = hydata["player"]["displayname"]
+# print(onHypixel)
+# print(onSkyblock)
 
-#     logoutime = hydata['player']['lastLogout']
-#     logintime = hydata['player']['lastLogin']
-    
-#     if logoutime > logintime:
-#         print(f"{player} is no longer online")
-#         break
-#     time.sleep(60)
+
+#Main runtime
+while True:
+    requestStatus = f"https://api.hypixel.net/status?key={api_key}&uuid={uuid}"
+
+    hydata = requests.get(requestStatus).json()
+    onHypixel = hydata["session"]["online"]
+    # onSkyblock = hydata["session"]["gameType"]
+
+    if onHypixel == True:
+        if hydata["session"]["gameType"] != "SKYBLOCK" and hydata["session"]["mode"] != "dynamic":
+            print("The player is no longer playing Skyblock")
+            break
+    if onHypixel == False:
+        print("The player has been disconnected from Hypixel")
+        break
+    time.sleep(60)
     
