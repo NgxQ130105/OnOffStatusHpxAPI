e75de94e22162240d47d8a56f7939d0d736de2b8
Quang Nguyen <quang13012005@gmail.com>
2022-03-17 17:12:43 +0700

    Final 17.3 changes

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 #Import libs
-from asyncio import events
 import asyncio
 import aiohttp #Replacing request module with aiohttp
 from mojang import MojangAPI #Mojang api to get Json API Data 
 import hikari #Hikari module for Python Discord bot
 import lightbulb #Lightbulb extension for more Hikari Utils
+from lightbulb.ext import tasks
 
 
 bot = lightbulb.BotApp(
-    token="OTUyNDYzOTM1NjIwMTI0NzEy.Yi2ZKg.2_ewOLMXxS_QqWw3GwGrMnSsai0",
+    token="",
     default_enabled_guilds=(952060009863344148)
 )
 
@@ -18,31 +18,29 @@ async def on_started(event):
     print('Bot has started running!')
 
 #Create Pinging request to Discord bot using Slash Command (Decorator)
-@bot.listen()
-async def ping(event: hikari.GuildMessageCreateEvent) -> None:
-    if event.is_bot or not event.content:
-        return
+@bot.command
+@lightbulb.command('afk', 'Checking AFK condition of the player')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond('Hello, you will be notified when you are logged out')
+    api_key = "00297e7c-8c63-4e7c-8ce3-aa8a419e3fd0"
     
-    if event.content.startswith("hk.ping"):
-        await event.message.respond('Hello, you will be notified when you are logged out')
-        api_key = "00297e7c-8c63-4e7c-8ce3-aa8a419e3fd0"
-        
-        userinput = "z3r0o2" #or input()
+    userinput = "z3r0o2" #or input()
 
+    
+    #Getting the Hpx ID
+    uuid = MojangAPI.get_uuid(userinput)
+    
+    statusURL = f"https://api.hypixel.net/status?key={api_key}&uuid={uuid}"
+    
+    playerURL = f"https://api.hypixel.net/player?key={api_key}&uuid={uuid}"
+    
+    async with aiohttp.ClientSession() as session1:
+        async with session1.get(playerURL) as response1:
+            data1 = await response1.json() #request Data API from api.hypixel.net
         
-        #Getting the Hpx ID
-        uuid = MojangAPI.get_uuid(userinput)
-        
-        statusURL = f"https://api.hypixel.net/status?key={api_key}&uuid={uuid}"
-        
-        playerURL = f"https://api.hypixel.net/player?key={api_key}&uuid={uuid}"
-        
-
-        async with aiohttp.ClientSession() as session1:
-            async with session1.get(playerURL) as response1:
-                data1 = await response1.json() #request Data API from api.hypixel.net
-            
-        playerName = data1["player"]["displayname"]
+    playerName = data1["player"]["displayname"]
+    while True:
 
         #Website API (Json Parametters)
         async with aiohttp.ClientSession() as session:
@@ -54,10 +52,14 @@ async def ping(event: hikari.GuildMessageCreateEvent) -> None:
             onSkyblock = data["session"]["gameType"] #get gameType from session from json Dict
             onWhichIs = data["session"]["mode"] #get mode from session from json Dict
             if onSkyblock != "SKYBLOCK" and onWhichIs != "dynamic":
-                await event.message.respond(f'{playerName} is no longer playing Hypixel Skyblock or no longer online')
-        if onHypixel == False:
-            await event.message.respond(f'{playerName} is no longer playing Hypixel Skyblock or no longer online')
-        asyncio.sleep(60)
+                await ctx.respond(f'{playerName} is no longer playing Hypixel Skyblock or no longer online')
         
-bot.run()
-
+        if onHypixel == False:
+            await ctx.respond(f'{playerName} is no longer playing Hypixel Skyblock or no longer online')
+        
+        await asyncio.sleep(0)
+        
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    task_one = loop.create_task(ping())
+    loop.run_forever()
